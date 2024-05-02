0e8b9293be88d45d860acab1b5de467c83c6085e
harishgurram <gurramharish275@gmail.com>
2024-05-02 08:26:16 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+st.title("🔎 Tell Me About this Car")
+"""
+Please give me an image, which has a `car` in it, and I will tell you more information about it.
+"""
+
+
