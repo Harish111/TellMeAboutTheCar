d86d1f96612e90094ac6dfd2379662387de4837a
harishgurram <gurramharish275@gmail.com>
2024-05-07 08:12:38 +0530

    Completed Stage 1, Able to upload a car image and get basic info using Google Gemini Vision API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#invoice extractor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,7 +7,9 @@ import streamlit as st
 import os
 from PIL import Image
 import google.generativeai as genai
+
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 
 def get_gemini_resonse(input,image, prompt):
@@ -26,24 +29,22 @@ def input_image_setup(upload_file):
     else:
          raise FileNotFoundError("No file uploaded")
      
-
-def main():
-
-    st.title("🔎 Tell Me About this Car")
-    """
-    Please give me an image, which has a `car` in it, and I will tell you more information about it.
-    """
-    upload_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
-    image = ""
-    if upload_file is not None:
-        image = Image.open(upload_file)
-        st.image(image, caption="uploaded image.", use_column_width=True)
-        
-    submit = st.button("tell me about the invoice")
+st.set_page_config(page_title= "Tell me about the car")
+st.title("🔎 Tell Me About this Car")
     
-    input_prompt = """You are an expert in understanding Automobiles
+input = "Tell me about this car and MAKE SURE THE RESPONSE IS IN GOOD FORMATE and EASY TO UNDERSTAND"
+upload_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
+image = ""
+if upload_file is not None:
+    image = Image.open(upload_file)
+    st.image(image, caption="uploaded image.", use_column_width=True)
+    
+submit = st.button("tell me about the car")
+
+input_prompt = """You are an expert in understanding Automobiles
     You will receive input images of cars
     you will have to identify what is the car in the image and provid basic information about it
+    If there is no car in the image, then simply say `Please Upload an Image that has Car in it`
     
     
     ###RESPONSE###
@@ -57,12 +58,10 @@ def main():
     Market Share in India : 6.2%
     """
     
-    if submit:
-        image_data = input_image_setup(upload_file=upload_file)
+    
+
+if submit:
+    image_data = input_image_setup(upload_file=upload_file)
     response= get_gemini_resonse(input_prompt,image_data,input)
     st.subheader("The response is")
     st.write(response)
-
-if __name__ == "__main__":
-    main()
-
